04c42c4436faf086cded5c39cac84916e5246455
Opiyo24 <gabbynyanor@gmail.com>
2023-11-02 22:00:25 +0300

    Getters anmd setters

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -9,9 +9,25 @@ class Square:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
+        
+        @property
+        def size(self):
+            return (self.__size)
+        
+        @size.setter
+        def size(self, value):
+            if not isinstance(value, int):
+                raise TypeError("size must be an integer")
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = value
+
     
     def area(self):
         """
         Area ya square
         """
         return(self.__size * self.__size)
+
+one = Square(5)
+one.area()
