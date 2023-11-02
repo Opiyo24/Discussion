95bf5921f4ef2b76943103771c80dc12d99a1277
Opiyo24 <gabbynyanor@gmail.com>
2023-11-02 21:16:59 +0300

    Area

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -4,8 +4,17 @@ class Square:
     comments
     """
     def __init__(self, size=0):
-        self.size = size #private instance attribute
+        self.__size = size #private instance attribute
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
+    
+    def area(self):
+        """
+        Area ya square
+        """
+        return(self.__size * self.__size)
+
+one = Square(5)
+print(one.area())
